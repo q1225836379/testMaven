95145010d1ed4858035e7b6ab7e9f210556dd079
unknown <1225836379@qq.com>
2019-12-19 16:14:40 +0800

    试一试

---
+++ com.easyui/src/main/java/com/easyui/test.java
@@ -0,0 +1,25 @@
+package com.easyui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
+@SpringBootApplication
+
+
+public class test {
+
+
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(test.class, args);
+	}
+
+}
